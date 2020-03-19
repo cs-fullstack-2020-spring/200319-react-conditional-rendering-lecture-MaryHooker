6933e3e79f6ed33f85f7729b8da498949bd181c6
MaryHooker <mc020791@yahoo.com>
2020-03-19 10:32:41 -0500

    using conditionals to render seperate forms

--- conditional-rendering-lecture-app/src/components/AppContainer.jsx
+++ conditional-rendering-lecture-app/src/components/AppContainer.jsx
@@ -5,15 +5,73 @@ import Login from './Login';
 class AppContainer extends Component {
     constructor(props) {
         super(props);
-        this.state = {}
+        this.state = {
+            isLoggingIn: false,
+            isSigningUp: false,
+        }
     }
+
+    updateIsLoggingIn = () => {
+        // //sanity
+        // console.log('Login button is clicked');
+
+        this.setState(
+            {
+                isLoggingIn: true
+            }
+        )
+
+        this.setState(
+            {
+                isSigningUp: false
+            }
+        )
+    }
+
+    updateIsSigningUp = () => {
+        // //sanity
+        // console.log('Signup button is clicked');
+
+        this.setState(
+            {
+                isSigningUp: true
+            }
+        )
+
+        this.setState(
+        {
+            isLoggingIn: false
+        }
+        )
+    }
+
     render() {
+        let userForm;
+
+        if(this.state.isLoggingIn){
+            //render the log in component
+            userForm = <Login/>
+        } else if(this.state.isSigningUp){
+            //render the sign up
+            userForm= <SignUp/>
+        }
+
         return (
             <div>
                 <h1>20-03-19 React Conditional Rendering Lecture</h1>
-                <Login />
-                <br />
-                <SignUp />
+                <br/>
+                <button onClick={this.updateIsLoggingIn} className='button'>Login</button>
+                <br/>
+                <br/>
+                <button onClick={this.updateIsSigningUp} className='button'>Sign Up</button>
+               <br/>
+
+                <div className='userForm'>
+                {userForm}
+                </div>
+               
+               
+                
             </div>
         );
     }
