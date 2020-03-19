d5eff0eac42b6377f5d838cd7ab1078e1da4b33e
MaryHooker <mc020791@yahoo.com>
2020-03-19 10:07:23 -0500

    login form created

--- conditional-rendering-lecture-app/src/components/AppContainer.jsx
+++ conditional-rendering-lecture-app/src/components/AppContainer.jsx
@@ -1,21 +1,22 @@
-import React,{Component} from 'react';
+import React, { Component } from 'react';
 import SignUp from './SignUp';
 import Login from './Login';
 
 class AppContainer extends Component {
     constructor(props) {
         super(props);
-        this.state = {  }
+        this.state = {}
     }
-    render() { 
-        return ( 
-        <div>
-            <h1>20-03-19 React Conditional Rendering Lecture</h1>
-            <SignUp/>
-            <Login/>
-        </div> 
+    render() {
+        return (
+            <div>
+                <h1>20-03-19 React Conditional Rendering Lecture</h1>
+                <Login />
+                <br />
+                <SignUp />
+            </div>
         );
     }
 }
- 
+
 export default AppContainer;
--- conditional-rendering-lecture-app/src/components/Login.jsx
+++ conditional-rendering-lecture-app/src/components/Login.jsx
@@ -1,17 +1,64 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 
 class Login extends Component {
     constructor(props) {
         super(props);
-        this.state = {  }
+        this.state = {
+
+            userName: '',
+            password: '',
+
+        }
     }
-    render() { 
-        return ( 
+
+    handleChange = (event) => {
+
+        if (event.target.name === 'userName') {
+            this.setState(
+                {
+                    userName: event.target.value
+                }
+            )
+        } else if (event.target.name === 'password') {
+            this.setState(
+                {
+                    password: event.target.value
+                }
+            )
+        }
+
+    }
+
+    handleSubmission = (event) => {
+        event.preventDefault();
+        console.log(this.state);
+    }
+
+    render() {
+        return (
             <div>
-                <h1>Login</h1>
+                <form action="">
+                    <fieldset className='border'>
+                        <legend className='legend'>Login</legend>
+
+
+                        <div className='formGroup'>
+                            <label htmlFor="userName">User Name: </label>
+                            <input type="text" name='userName' id='userName' value={this.state.userName} onChange={this.handleChange} className='input' />
+                        </div>
+
+                        <div className='formGroup'>
+                            <label htmlFor="password">Password: </label>
+                            <input type="password" name='password' id='password' value={this.state.password} onChange={this.handleChange} className='input' />
+                        </div>
+
+                        <button onClick={this.handleSubmission} className='button'>Login</button>
+
+                    </fieldset>
+                </form>
             </div>
-         );
+        );
     }
 }
- 
+
 export default Login;
